cfa9686aeff2be5a4cc16ffe194611eb5797681f
Lior Kovtun <47320608+Lihexs@users.noreply.github.com>
2023-11-19 13:48:54 +0200

    Update statements.py

--- backend/flask/server/api/statements.py
+++ backend/flask/server/api/statements.py
@@ -3,7 +3,7 @@ from flask import jsonify, request, make_response
 from db import mongo
 from .utils import parse_json
 class Statements(Resource):
-    def post(self):
+    def get(self):
         data = request.get_json()
         level_id = data['level_id']
         statements = mongo.Statements.get_level_statements(level_id=level_id)
